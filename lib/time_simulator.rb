13f890316b216dfda4a14f23d9cb813813b214df
Jake Prime <jake@jakeprime.com>
2025-03-06 17:40:41 +0000

    Introduce new time helper methods
    Makes sense to have `in_up_to` as opposed to `in_about`. For example CI is
    always going to take about 10 minutes, but someone can branch and commit at a
    much more random frequency.

--- lib/time_simulator.rb
+++ lib/time_simulator.rb
@@ -4,10 +4,19 @@ module TimeSimulator
   TIME_SCALE = 1.0 / 300
 
   def in_about(time)
-    # randomly vary from 50% to 150% of given time value
-    random_variation = (Random.rand) + 0.5
+    # randomly vary from 75% to 125% of given time value
+    random_variation = (Random.rand * 0.5) + 0.75
     sleep(time * TIME_SCALE * random_variation)
 
     yield
   end
+
+  def in_up_to(time)
+    # randomly vary from 50% to 150% of given time value
+    random_variation = Random.rand
+    sleep(time * TIME_SCALE * random_variation)
+
+    yield
+  end
+
 end
